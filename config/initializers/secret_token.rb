e2a87217d5cf0e75f5da27f8f1d19406c228c0fc
Special Leung <specialcyci@gmail.com>
2014-04-25 19:41:06 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EasyExchange::Application.config.secret_key_base = 'a2eb5632761eda76dbeaec8b5aa938e0bccf97dfd8c089ae55eac316a31c3ba432c89f4a62ccb9a6670a6978071eee6b1cd0608947b4e7cca2d7aa2a4938a797'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EasyExchange::Application.config.session_store :cookie_store, key: '_EasyExchange_session'
